2473513d2324565d4086fd541b6df711601f1691
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-09 01:16:41 +0900

    Update Ball.cs
    Add Increase Logic

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,9 +2,41 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    private string blueTag = "Blue";
+    private string orangeTag = "Orange";
+    private bool canSpawn = true;
+    private float spawnDelay = 1.0f; // 생성 딜레이 시간
+
+    private void OnEnable()
+    {
+        canSpawn = false;
+        Invoke(nameof(ResetSpawn), spawnDelay);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (canSpawn && other.CompareTag(blueTag))
+        {
+            canSpawn = false; // 생성 후 일정 시간 동안 재생성 방지
+            for (int i = 0; i < other.GetComponent<Obstacle>().MyNum; i++)
+            {
+                GameObject obj = ObjectPool.SpawnFromPool("Ball", transform.position, Quaternion.identity);
+                obj.GetComponent<Rigidbody>().AddForce(Vector3.down);
+            }
+            ObjectPool.ReturnToPool(gameObject);
+            CancelInvoke();
+            //Invoke(nameof(ResetSpawn), spawnDelay);
+        }
+    }
+
+    private void ResetSpawn()
+    {
+        canSpawn = true;
+    }
+
     private void OnDisable()
     {
-        ObjectPool.ReturnToPool(gameObject);    // 한 객체에 한번만
+        ObjectPool.ReturnToPool(gameObject);
         CancelInvoke();
     }
 }
