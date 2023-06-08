b61a1020c1030323e109a3abe1e7e9e256fc2bbe
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-09 01:15:21 +0900

    Create Obstacle.cs
    It includes WorldSpace UI for multiply num

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,18 @@
+using TMPro;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI myText = null;
+    private int myNum;
+    public int MyNum { get { return myNum; } }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        myText = GetComponentInChildren<TextMeshProUGUI>();
+        int rand = Random.Range(2, 4);
+        myText.text = $"X {rand}";
+        myNum = rand;
+    }
+}
