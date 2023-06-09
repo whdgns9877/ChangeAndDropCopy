ec95a697e80bfe826c842835aeca885ee268dda9
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-10 01:05:41 +0900

    Update Ball.cs

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -22,42 +22,90 @@ public class Ball : MonoBehaviour
         canSpawn = false;
         // 일정시간 이후에 생성할수 있는상태로 바꿔준다
         Invoke(nameof(ResetSpawn), spawnDelay);
-        // RigidBody컴포넌트를 얻고 
+        //// RigidBody컴포넌트를 얻고 
         myRb = GetComponent<Rigidbody>();
-        // 활성화되면 우선 아래로 발사
+        //// 활성화되면 우선 아래로 발사
         GoDown();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        // 현재 스폰이 가능한 상태이고 
-        if (canSpawn && other.CompareTag(Utils.BLUETAG))
+        // 공들이 파란색 상태일때
+        if(PlayerInput.isBallBlue == true)
         {
-            canSpawn = false; // 생성 후 일정 시간 동안 재생성 방지
-            int createNum = other.GetComponent<Obstacle>().MyNum; // 본인이 충돌한 물체의 MyNum으로 몇개 생성할지 결정
-
-            int increase = GetIncreaseAmount(createNum); // GetIncreaseAmount함수를 통해 (0,4) ,(0,2,4), (0,1,2,3,4) 선택
-
-            int idx = 0;
-
-            for (int i = 0; i < createNum; ++i)
+            // 스폰 가능하고 충돌체가 파란 태그라면
+            if(canSpawn && other.CompareTag(Utils.BLUETAG))
             {
-                // ObjectPool에서 오브젝트 활성화 (꺼낼게 없으면 생성함)
-                ObjectPool.SpawnFromPool(Utils.BALLTAG, transform.position + spawnPoints[idx], Quaternion.identity);
-                idx += increase;
+                canSpawn = false; // 생성 후 일정 시간 동안 재생성 방지
+                int createNum = other.GetComponent<Obstacle>().MyNum; // 본인이 충돌한 물체의 MyNum으로 몇개 생성할지 결정
+
+                int increase = GetIncreaseAmount(createNum); // GetIncreaseAmount함수를 통해 (0,4) ,(0,2,4), (0,1,2,3,4) 선택
+
+                int idx = 0;
+
+                for (int i = 0; i < createNum; ++i)
+                {
+                    // ObjectPool에서 오브젝트 활성화 (꺼낼게 없으면 생성함)
+                    ObjectPool.SpawnFromPool(Utils.BALLTAG, transform.position + spawnPoints[idx], Quaternion.identity);
+                    idx += increase;
+                }
+
+                // 전부 생성시켜줬다면 생성시킨 주체는 Pool에 반환시킴
+                ObjectPool.ReturnToPool(gameObject);
+                // 실행하고 있던 모든 로직 종료
+                CancelInvoke();
             }
 
-            // 전부 생성시켜줬다면 생성시킨 주체는 Pool에 반환시킴
-            ObjectPool.ReturnToPool(gameObject);
-            // 실행하고 있던 모든 로직 종료
-            CancelInvoke();
+            // 충돌체가 오랜지 태그라면
+            if (other.CompareTag(Utils.ORANGETAG))
+            {
+                // Pool에 반환
+                ObjectPool.ReturnToPool(gameObject);
+                // 실행하고 있던 모든 로직 종료
+                CancelInvoke();
+            }
+        }
+        // 공들이 주황색 상태일때
+        else
+        {
+            // 스폰 가능하고 충돌체가 파란 태그라면
+            if (canSpawn && other.CompareTag(Utils.ORANGETAG))
+            {
+                canSpawn = false; // 생성 후 일정 시간 동안 재생성 방지
+                int createNum = other.GetComponent<Obstacle>().MyNum; // 본인이 충돌한 물체의 MyNum으로 몇개 생성할지 결정
+
+                int increase = GetIncreaseAmount(createNum); // GetIncreaseAmount함수를 통해 (0,4) ,(0,2,4), (0,1,2,3,4) 선택
+
+                int idx = 0;
+
+                for (int i = 0; i < createNum; ++i)
+                {
+                    // ObjectPool에서 오브젝트 활성화 (꺼낼게 없으면 생성함)
+                    ObjectPool.SpawnFromPool(Utils.BALLTAG, transform.position + spawnPoints[idx], Quaternion.identity);
+                    idx += increase;
+                }
+
+                // 전부 생성시켜줬다면 생성시킨 주체는 Pool에 반환시킴
+                ObjectPool.ReturnToPool(gameObject);
+                // 실행하고 있던 모든 로직 종료
+                CancelInvoke();
+            }
+
+            // 충돌체가 오랜지 태그라면
+            if (other.CompareTag(Utils.BLUETAG))
+            {
+                // Pool에 반환
+                ObjectPool.ReturnToPool(gameObject);
+                // 실행하고 있던 모든 로직 종료
+                CancelInvoke();
+            }
         }
     }
 
     // 아랫방향으로 랜덤방향으로 힘을 가한다
     private void GoDown()
     {
-        myRb.velocity = Vector3.down;
+        //myRb.velocity = Vector3.down;
         Vector3 dir = new Vector3(Random.Range(-1, 1f), -1f, 0f).normalized;
         myRb.AddForce(dir, ForceMode.Impulse);
     }
