8988dfebec1d41518236980c2e378fc38147c2b9
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-12 05:05:44 +0900

    Update Reset.cs

--- Assets/Scripts/Reset.cs
+++ Assets/Scripts/Reset.cs
@@ -11,10 +11,10 @@ public class Reset : MonoBehaviour
     // 게임 상태를 초기화하는 함수
     private void ResetGameState()
     {
-        Debug.Log("게임 초기 상태로 시작");
         PlayerInput.state = TouchState.NONE;
         PlayerInput.isBallBlue = true;
         PlayerInput.ballDrop = false;
+        PlayerInput.RunEvent();
     }
 
     // 현재 진행 중인 씬(게임 씬)을 다시 로드하여 처음 상태로 되돌리는 함수
