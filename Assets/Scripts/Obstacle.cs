95164d954eb138a0550bdad4e56ce5ebcbe177a2
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-11 23:01:38 +0900

    Update Obstacle.cs

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,20 +1,4 @@
-using TMPro;
-using UnityEngine;
-
-public class Obstacle : MonoBehaviour
+public interface IObstacle
 {
-    // 앞에 보일 Text
-    [SerializeField] private TextMeshProUGUI myText = null;
-    // 몇개를 곱할것인지
-    private int myNum;
-    public int MyNum { get { return myNum; } }
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        myText = GetComponentInChildren<TextMeshProUGUI>(); // TextMeshPro 참조를 얻고
-        int rand = Random.Range(2, 5); // 2~4 랜덤숫자
-        myText.text = $"X {rand}"; // text에 랜덤숫자 결과 넣고
-        myNum = rand; // myNum에 대입
-    }
+    public void Process();
 }
