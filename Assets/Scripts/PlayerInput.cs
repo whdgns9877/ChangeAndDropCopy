5c2b677752188f512bfec0917d93322c65509764
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-11 23:00:23 +0900

    Update PlayerInput.cs
    Add State None and Create Method the Common Func

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,12 +1,12 @@
 using System;
 using UnityEngine;
 
-public enum TouchState { TouchBegan, TouchEnd, SingleTouch, Swipe }
+public enum TouchState { TouchBegan, TouchEnd, SingleTouch, Swipe, NONE }
 
 public static class PlayerInput
 {
     // 플레이어의 입력을 나타내줄 enum
-    public static TouchState state = TouchState.TouchEnd;
+    public static TouchState state = TouchState.NONE;
 
     public static bool ballDrop = false; // 처음 터치로 공들을 쏟았는지를 나타내줄 bool 변수
     public static bool isBallBlue = true; // 볼 드랍 이후 터치시 blue/orange로 색이 변하는데 이것을 나타내줄 변수
@@ -21,57 +21,93 @@ public static class PlayerInput
     // Monobehaviour의 Update함수가 아니므로 PlayerInput함수를 이용하는 측에서 해당 Update함수를 호출해야한다
     public static void Update()
     {
+#if UNITY_EDITOR
+        // 에디터에서 터치 입력 시뮬레이션
+        if (Input.GetMouseButtonDown(0))
+        {
+            InputStart();
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            InputMove();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            InputEnd();
+        }
+        else
+        {
+            state = TouchState.NONE;
+        }
+#else
+        // 모바일 디바이스에서 터치 입력
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
 
-            // 터치 시작시
             if (touch.phase == TouchPhase.Began)
             {
-                state = TouchState.TouchBegan;
-                isSwiping = false;
-                swipeStartPosition = touch.position;
-                swipeDistance = 0f;
+                InputStart();
             }
-            // 터치 시작 이후 스와이프로 움직였을시
             else if (touch.phase == TouchPhase.Moved)
             {
-                // 스와이프 중이 아니고 스와이프한 거리가 최소거리를 넘었을때
-                if (!isSwiping && Vector2.Distance(touch.position, swipeStartPosition) >= swipeThreshold)
-                {
-                    isSwiping = true;
-                    Vector2 swipeDirection = touch.position - swipeStartPosition;
-                    float swipeAngle = Vector2.Angle(Vector2.right, swipeDirection);
-                    state = TouchState.Swipe;
-                }
-
-                // 스와이프 중일때
-                if (isSwiping)
-                {
-                    // 값이 커서 0.01을 곱함
-                    swipeDistance = (touch.position.x - swipeStartPosition.x) * 0.01f;
-                }
+                InputMove();
             }
-            // 터치가 끝나거나 중간에 떼졌을경우(손가락이 터치 범위밖으로 나가거나 실수로 떼어졌을때 등)
             else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
             {
-                // 스와이프 중이었다면
-                if (isSwiping)
-                {
-                    state = TouchState.TouchEnd;                   
-                    ballDrop = true;
-                }
-                // 스와이프 중이 아니고 이미 공들이 떨어지고 있다면
-                else if(ballDrop == true)
-                {
-                    state = TouchState.SingleTouch;
-                    isBallBlue = !isBallBlue;
-                    OnSingleTouch?.Invoke(); //등록해놓은 Event 발생 (공들의 색 변화)
-                }
+                InputEnd();
             }
+        }
+        else
+        {
+            state = TouchState.NONE;
+        }
+#endif
+    }
+
+    // 입력이 시작되었을때 실행할 함수
+    private static void InputStart()
+    {
+        state = TouchState.TouchBegan;
+        isSwiping = false;
+        swipeStartPosition = Input.mousePosition;
+        swipeDistance = 0f;
+    }
+
+    // 입력이 발생하고 움직일때 실행할 함수
+    private static void InputMove()
+    {
+        if (!isSwiping && Vector2.Distance(Input.mousePosition, swipeStartPosition) >= swipeThreshold)
+        {
+            isSwiping = true;
+            Vector2 swipeDirection = (Vector2)Input.mousePosition - swipeStartPosition;
+            float swipeAngle = Vector2.Angle(Vector2.right, swipeDirection);
+            state = TouchState.Swipe;
+        }
+
+        if (isSwiping)
+        {
+            swipeDistance = (Input.mousePosition.x - swipeStartPosition.x) * 0.01f;
         }
     }
 
+    // 입력이 끝났을때 실행할 함수
+    private static void InputEnd()
+    {
+        if (isSwiping)
+        {
+            state = TouchState.TouchEnd;
+            ballDrop = true;
+        }
+        else if (ballDrop == true)
+        {
+            state = TouchState.SingleTouch;
+            isBallBlue = !isBallBlue;
+            OnSingleTouch?.Invoke();
+        }
+    }
+    
+    // 스와이프 거리를 Clamping하여 반환하는 함수
     public static float GetSwipeDistance()
     {
         return Mathf.Clamp(swipeDistance, -3.18f, 3.18f);
