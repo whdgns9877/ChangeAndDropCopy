8e524db219713a27f9f52bd9ac2fb9c2f3d312d4
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-12 05:04:23 +0900

    Update CameraMove.cs

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -7,6 +7,8 @@ public class CameraMove : MonoBehaviour
 
     private float offsetY = 10f;
 
+    private float minY = Mathf.Infinity; // 첫비교는 가장 큰수로 하여 최솟값을 못찾는일이 없도록 설정
+
     // Update is called once per frame
     private void Update()
     {
@@ -15,7 +17,13 @@ public class CameraMove : MonoBehaviour
         {
             targetTr = Utils.FindTarget(); // 타겟을 찾고
             if (targetTr == null) return;
-            transform.position = new Vector3(transform.position.x, targetTr.position.y + offsetY, transform.position.z); // y축만 이동시킨다
+            // 제일 작은 y값을 기억해놓고 실시간으로 변하는 target들중에서 최솟값보다 작은값이 없다면 저장되어있던
+            // minY값을 그대로 사용
+            if(minY > targetTr.transform.position.y)
+            {
+                minY = targetTr.transform.position.y;
+            }
+            transform.position = new Vector3(transform.position.x, minY + offsetY, transform.position.z); // y축만 이동시킨다
         }
     }
 }
