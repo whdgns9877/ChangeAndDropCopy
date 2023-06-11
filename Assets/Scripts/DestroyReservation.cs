9a5a6db7cd4105b79a3f7faa5d10eb3879cedb30
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-12 05:04:41 +0900

    Creaet DestroyReservation.cs
    for auto destroy

---
+++ Assets/Scripts/DestroyReservation.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class DestroyReservation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start() => Destroy(gameObject, 2.0f);
+    
+}
